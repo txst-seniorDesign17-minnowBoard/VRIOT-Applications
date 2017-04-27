912b0067b0a4225f5d0416cc8b87f74dfee79466
collin <cgw21@txstate.edu>
2017-04-26 22:24:48 -0500

    refactor

--- Amazon-Flask-Ask/Demo1.py
+++ Amazon-Flask-Ask/Demo1.py
@@ -22,3 +22,12 @@ def start_skill():
 
 @ask.intent("YesIntent")
 def say_bestie();
+    bestie = "The way the legend of the Source God goes is ... There once was a man "
+
+@ask.intent("NoIntent")
+def no_bestie():
+    randy = "Your name is probably Randy and this does not change things"
+    return (randy)
+
+if __name__ == 'main':
+    app.run(debug=True, host='0.0.0.0', port='80')
