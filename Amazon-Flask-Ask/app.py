404a2e3f80cac67764c596d6fe90e51c2c546247
collin <cgw21@txstate.edu>
2017-04-26 21:56:06 -0500

    refactor

---
+++ Amazon-Flask-Ask/app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_ask import Ask, statement, question, session
+import json
+import requests
+import time
+import unidecode
+
+app = Flask(__name__)
+ask = Ask(app, "/")
