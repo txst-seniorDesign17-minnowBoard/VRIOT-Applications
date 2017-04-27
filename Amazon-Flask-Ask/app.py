7db2c33498879d5742e98da9ca4c75b51cb7fc5d
collin <cgw21@txstate.edu>
2017-04-26 22:11:11 -0500

    refactor

--- Amazon-Flask-Ask/app.py
+++ Amazon-Flask-Ask/app.py
@@ -7,3 +7,18 @@ import unidecode
 
 app = Flask(__name__)
 ask = Ask(app, "/")
+
+def get_data():
+    pass
+
+@app.route('/')
+def homepage():
+    return "Senior Design Demo"
+
+@ask.launch
+def start_skill():
+    welcome_message = "Hello, Welcome to the MinnowBoard Senior Design Project...Would you like to know who Randy's Best Friend is?"
+    return question(welcome_message)
+
+@ask.intent("YesIntent")
+def say_bestie();
